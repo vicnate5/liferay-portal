1149566ffd4ac0a984e646346845fd27b61e58f8
Victor Ware <victor.ware@liferay.com>
2015-08-25 09:31:14 -0700

    Revert "LPS-58126 Revert "LPS-56015 We only want the regular parameters, see getParameterValues and other methods in the class.""
    This reverts commit 0dc86746f4239195007c133777dc3f6d90aa2bdb.

--- portal-impl/src/com/liferay/portal/upload/UploadServletRequestImpl.java
+++ portal-impl/src/com/liferay/portal/upload/UploadServletRequestImpl.java
@@ -462,7 +462,6 @@ public class UploadServletRequestImpl
 		}
 
 		parameterNames.addAll(_regularParameters.keySet());
-		parameterNames.addAll(_fileParameters.keySet());
 
 		return Collections.enumeration(parameterNames);
 	}
