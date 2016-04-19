72ac1f2055cba8eadd77b1f0072047763a966ea9
Bruno Basto <bruno.basto@liferay.com>
2016-04-19 13:31:40 -0300

    LPS-64917 Properly destroys form repeatable instances.

--- modules/apps/foundation/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/ddm_form.js
+++ modules/apps/foundation/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/ddm_form.js
@@ -332,6 +332,12 @@ AUI.add(
 						);
 					},
 
+					destructor: function() {
+						var instance = this;
+
+						instance.get('container').remove();
+					},
+
 					renderUI: function() {
 						var instance = this;
 
@@ -1955,12 +1961,11 @@ AUI.add(
 				NAME: 'liferay-ddm-form',
 
 				prototype: {
-					repeatableInstances: {},
-
 					initializer: function() {
 						var instance = this;
 
 						instance.eventHandlers = [];
+						instance.repeatableInstances = {};
 
 						instance.bindUI();
 						instance.renderUI();
@@ -1996,8 +2001,20 @@ AUI.add(
 						var instance = this;
 
 						AArray.invoke(instance.eventHandlers, 'detach');
+						AArray.invoke(instance.get('fields'), 'destroy');
+
+						instance.get('container').remove();
 
 						instance.eventHandlers = null;
+
+						 A.each(
+						 	instance.repeatableInstances,
+						 	function(item) {
+						 		item.destroy();
+						 	}
+						 );
+
+						 instance.repeatableInstances = null;
 					},
 
 					moveField: function(parentField, oldIndex, newIndex) {
