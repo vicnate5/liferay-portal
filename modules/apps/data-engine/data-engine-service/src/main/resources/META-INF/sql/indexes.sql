d4cfe1a527ad04324cd6c07c2f97e1392cd538f1
Alberto Chaparro <alberto.chaparro@liferay.com>
2019-10-21 13:01:19 -0700

    LPS-103034 buildService

--- modules/apps/data-engine/data-engine-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/data-engine/data-engine-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,4 +1,5 @@
 create unique index IX_2CEE588F on DEDataDefinitionFieldLink (classNameId, classPK, ddmStructureId, fieldName[$COLUMN_LENGTH:75$]);
+create index IX_5145BB70 on DEDataDefinitionFieldLink (uuid_[$COLUMN_LENGTH:75$], companyId);
 create unique index IX_AAE65DF2 on DEDataDefinitionFieldLink (uuid_[$COLUMN_LENGTH:75$], groupId);
 
 create index IX_FA1639C7 on DEDataListView (groupId, companyId, ddmStructureId);
