51cbafa59d231338a45aa287086152ddda8ffc77
Victor Ware <victor.ware@liferay.com>
2019-11-01 11:41:46 -0700

    LPS-101690 Ignore the jmx test case

--- modules/apps/portal/portal-jmx-test/src/testIntegration/java/com/liferay/portal/jmx/test/JMXWhiteboardTest.java
+++ modules/apps/portal/portal-jmx-test/src/testIntegration/java/com/liferay/portal/jmx/test/JMXWhiteboardTest.java
@@ -32,6 +32,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -44,6 +45,7 @@ import org.osgi.framework.ServiceRegistration;
 /**
  * @author Raymond Augé
  */
+@Ignore
 @RunWith(Arquillian.class)
 public class JMXWhiteboardTest {
 
