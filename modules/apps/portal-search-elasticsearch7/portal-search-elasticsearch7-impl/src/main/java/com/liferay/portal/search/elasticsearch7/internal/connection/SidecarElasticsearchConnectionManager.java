ca99b3513b4bb308725051e79eca5683fd84b2fc
tina.tian <tina.tian@liferay.com>
2020-02-04 22:06:13 -0800

    LPS-103577 portal-search-elasticsearch7-impl: add warning message, do not use sidecar in production

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/connection/SidecarElasticsearchConnectionManager.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/connection/SidecarElasticsearchConnectionManager.java
@@ -16,6 +16,7 @@ package com.liferay.portal.search.elasticsearch7.internal.connection;
 
 import com.liferay.petra.concurrent.FutureListener;
 import com.liferay.petra.process.ProcessExecutor;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.cluster.ClusterExecutor;
 import com.liferay.portal.kernel.log.Log;
@@ -62,6 +63,21 @@ public class SidecarElasticsearchConnectionManager {
 		if (_elasticsearchConfiguration.operationMode() ==
 				com.liferay.portal.search.elasticsearch7.configuration.
 					OperationMode.EMBEDDED) {
+
+			if (_log.isWarnEnabled()) {
+				StringBundler sb = new StringBundler(8);
+
+				sb.append("Liferay is configured to use Elasticsearch engine ");
+				sb.append("running in a child process of current process ");
+				sb.append("named as sidecar. Do NOT use sidecar in ");
+				sb.append("production. Sidecar is useful for development and ");
+				sb.append("demonstration purposes. Refer to the ");
+				sb.append("documentation for details on the limitations of ");
+				sb.append("sidecar. Remote Elasticsearch connections can be ");
+				sb.append("configured in the Control Panel.");
+
+				_log.warn(sb.toString());
+			}
 
 			elasticsearchConnection = new SidecarElasticsearchConnection(
 				new Sidecar(
