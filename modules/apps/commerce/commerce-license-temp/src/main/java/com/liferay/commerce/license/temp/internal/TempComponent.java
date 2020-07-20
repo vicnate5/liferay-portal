f907d144b864fffad3bd517d3100cd1d432ca627
tina.tian <tina.tian@liferay.com>
2020-07-20 11:17:03 -0700

    LPS-X One simple test bundle.

---
+++ modules/apps/commerce/commerce-license-temp/src/main/java/com/liferay/commerce/license/temp/internal/TempComponent.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.license.temp.internal;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Tina Tian
+ */
+@Component(service = {})
+public class TempComponent {
+
+	@Activate
+	protected void activate() {
+		System.out.println("#### This is TempComponent activate");
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		System.out.println("#### This is TempComponent deactivate");
+	}
+
+}
