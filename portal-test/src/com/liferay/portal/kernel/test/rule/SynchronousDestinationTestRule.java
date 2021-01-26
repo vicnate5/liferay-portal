a0850c6001251a9da0bdacd285cc14ca35b07c9a
Alejandro Tardín <alejandro.tardin@liferay.com>
2021-01-26 15:14:33 +0100

    LPS-126777 Downloading synchronously in tests

--- portal-test/src/com/liferay/portal/kernel/test/rule/SynchronousDestinationTestRule.java
+++ portal-test/src/com/liferay/portal/kernel/test/rule/SynchronousDestinationTestRule.java
@@ -204,6 +204,7 @@ public class SynchronousDestinationTestRule
 			replaceDestination("liferay/shipment_status");
 			replaceDestination("liferay/stock_quantity");
 			replaceDestination("liferay/subscription_status");
+			replaceDestination("liferay/tensorflow_model_download");
 
 			if (_sync != null) {
 				for (String name : _sync.destinationNames()) {
