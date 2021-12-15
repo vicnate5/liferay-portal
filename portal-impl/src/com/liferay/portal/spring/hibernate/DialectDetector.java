11fc99f07e85b708afb034045ca1076aa9b61c7f
Zoltán Takács <zoltan.takacs@liferay.com>
2021-12-15 11:08:07 +0100

    LPS-143661 Simplify condition

--- portal-impl/src/com/liferay/portal/spring/hibernate/DialectDetector.java
+++ portal-impl/src/com/liferay/portal/spring/hibernate/DialectDetector.java
@@ -102,9 +102,7 @@ public class DialectDetector {
 			else if (dbName.startsWith("DB2") && (dbMajorVersion >= 9)) {
 				dialect = new DB2Dialect();
 			}
-			else if (StringUtil.startsWith(dbInfo.getDriverName(), "mariadb") &&
-					 StringUtil.startsWith(dbInfo.getUrl(), "jdbc:mariadb")) {
-
+			else if (StringUtil.startsWith(dbInfo.getUrl(), "jdbc:mariadb")) {
 				dialect = new MariaDBDialect();
 			}
 			else if (dbName.startsWith("Microsoft") && (dbMajorVersion == 9)) {
