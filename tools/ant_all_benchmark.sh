4cb438594d0dd689df5ae4d938ecd655375b7ceb
Alan Huang <alan.huang@liferay.com>
2022-01-12 20:03:37 +0800

    LPS-145170 Auto SF

---
+++ tools/ant_all_benchmark.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -e
+
+function echo_time {
+	local duration=${SECONDS}
+
+	echo "completed in $((${duration} / 60)) minutes and $((${duration} % 60)) seconds."
+}
+
+function main {
+	pushd .. > /dev/null
+
+	local binaries_cache_dir_name=liferay-binaries-cache-2020
+
+	if [ ! -e "../${binaries_cache_dir_name}" ]
+	then
+		echo "Clone https://github.com/liferay/${binaries_cache_dir_name} into ../${binaries_cache_dir_name} and rerun ${0}."
+
+		exit
+	fi
+
+	pushd ../${binaries_cache_dir_name} > /dev/null
+
+	git pull upstream master
+
+	popd > /dev/null
+
+	git clean -dfx -e "*.${USER}.*" > /dev/null
+
+	rm -fr ~/.liferay
+
+	ant -Dmirrors.hostname= -f build-dist.xml unzip-tomcat > /dev/null
+
+	ant setup-profile-dxp
+
+	echo ""
+	echo "Running \"ant all\" 3 times..."
+
+	run_ant_all
+
+	echo "Run 1 with a clean repository $(echo_time)"
+
+	rm -fr .gradle/caches
+
+	run_ant_all
+
+	echo "Run 2 without Gracle cache $(echo_time)"
+
+	run_ant_all
+
+	echo "Run 3 with all caches $(echo_time)"
+
+	popd > /dev/null
+}
+
+function run_ant_all {
+	if [ -e tools/gradle*.zip ]
+	then
+		./gradlew --stop
+	fi
+
+	SECONDS=0;
+
+	ant -Dmirrors.hostname= all > /dev/null
+	#sleep 5
+
+	echo ""
+}
+
+main "${@}"
