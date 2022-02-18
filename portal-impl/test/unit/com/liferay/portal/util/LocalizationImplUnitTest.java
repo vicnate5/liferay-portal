04f13f484c56ddc8a01b74a77ecf250a7e080f48
Tina Tian <tina.tian@liferay.com>
2022-02-18 15:09:13 -0800

    LPS-145927 Fix unit test, the LocaleUtil.fromLanguageId() used in this test is just used to translate a language Id into a locale, it should not either validate language or fall back to default locale.

--- portal-impl/test/unit/com/liferay/portal/util/LocalizationImplUnitTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/LocalizationImplUnitTest.java
@@ -98,7 +98,7 @@ public class LocalizationImplUnitTest {
 		Locale[] locale = new Locale[languageIds.length];
 
 		for (int i = 0; i < languageIds.length; i++) {
-			locale[i] = LocaleUtil.fromLanguageId(languageIds[i], false);
+			locale[i] = LocaleUtil.fromLanguageId(languageIds[i], false, false);
 		}
 
 		return locale;
@@ -142,7 +142,8 @@ public class LocalizationImplUnitTest {
 
 				}));
 
-		Locale locale = LocaleUtil.fromLanguageId(defaultContentLanguageId);
+		Locale locale = LocaleUtil.fromLanguageId(
+			defaultContentLanguageId, false, false);
 
 		LocaleUtil.setDefault(
 			locale.getLanguage(), locale.getCountry(), locale.getVariant());
@@ -151,7 +152,8 @@ public class LocalizationImplUnitTest {
 
 		localizationUtil.setLocalization(new LocalizationImpl());
 
-		Locale contentDefaultLocale = LocaleUtil.fromLanguageId("es_ES");
+		Locale contentDefaultLocale = LocaleUtil.fromLanguageId(
+			"es_ES", false, false);
 
 		Locale[] contentAvailableLocales = _getContentAvailableLocales(
 			"es_ES,en_US,de_DE");
@@ -163,13 +165,13 @@ public class LocalizationImplUnitTest {
 		if (expectedResult) {
 			Assert.assertTrue(
 				LocaleUtil.equals(
-					LocaleUtil.fromLanguageId(expectedLanguageId),
+					LocaleUtil.fromLanguageId(expectedLanguageId, false, false),
 					defaultImportLocale));
 		}
 		else {
 			Assert.assertFalse(
 				LocaleUtil.equals(
-					LocaleUtil.fromLanguageId(expectedLanguageId),
+					LocaleUtil.fromLanguageId(expectedLanguageId, false, false),
 					defaultImportLocale));
 		}
 	}
