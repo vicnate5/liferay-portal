4eed2d055f6cd1ea20fb937b256f090fddcf78f5
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-04-21 10:31:33 +0200

    LPS-145927 No need to validate locale since it is immediately done after, when checking if it is an available locale.

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -5012,7 +5012,7 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 
 		for (String languageId : languageIdsArray) {
 			if (!LanguageUtil.isAvailableLocale(
-					groupId, LocaleUtil.fromLanguageId(languageId))) {
+					groupId, LocaleUtil.fromLanguageId(languageId, false))) {
 
 				LocaleException localeException = new LocaleException(
 					LocaleException.TYPE_DISPLAY_SETTINGS);
