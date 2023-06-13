d7f0c58e318195076c998341ec568bbaf45b2dda
Susan Chen <susan.chen@liferay.com>
2023-06-13 16:40:33 -0700

    LPS-187398 Temp ignore tests until LPS-185851

--- modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/AutoUpgradeProcessTest.java
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/AutoUpgradeProcessTest.java
@@ -29,6 +29,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -124,6 +125,7 @@ public class AutoUpgradeProcessTest {
 		Assert.assertFalse(_upgradeProcessRun);
 	}
 
+	@Ignore
 	@Test
 	public void testUpgradeProcessWhenAutoUpgradeEnabled() throws Exception {
 		_releaseLocalService.addRelease(_SERVLET_CONTEXT_NAME, "1.0.0");
